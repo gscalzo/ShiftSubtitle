b689ca991c1a4ab404f5c04f33dad64ddc234002
J.Ordan <giordano@scalzo.biz>
2009-09-29 23:20:22 +0200

    add README with brief doc

--- spec/shift_subtitle_spec.rb
+++ spec/shift_subtitle_spec.rb
@@ -1,6 +1,4 @@
-$:.unshift('../lib')
-
-require 'shift_subtitle'
+require File.expand_path(File.dirname(__FILE__) + '/../lib/shift_subtitle')
 
 describe ShiftSubtitle do
 	it "should print usage when called without parameters" do
