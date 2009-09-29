63f43c9d7521de4a00d168ac61664ca8bc1170f2
J.Ordan <giordano@scalzo.biz>
2009-09-29 23:07:04 +0200

    add bare structure

---
+++ lib/shift_subtitle.rb
@@ -0,0 +1,3 @@
+class ShiftSubtitle
+	
+end
---
+++ spec/shift_subtitle_spec.rb
@@ -0,0 +1,9 @@
+$:.unshift('../lib')
+
+require 'shift_subtitle'
+
+describe ShiftSubtitle do
+	it "should print usage when called without parameters" do
+	
+	end
+end
