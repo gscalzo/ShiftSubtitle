1632abe932bd9fe943cd24b0aa9d9b4aab3d473b
gscalzo <gscalzo@nkgscalzp2.urmetcns.it>
2009-09-30 15:43:05 +0200

    implemented a bare second spec: need to refactor

--- lib/shift_subtitle.rb
+++ lib/shift_subtitle.rb
@@ -1,3 +1,12 @@
 class ShiftSubtitle
-	
+	attr :operation
+
+	def initialize(messenger)
+		@messenger = messenger
+	end
+
+	def start(argv=[])
+		@messenger.puts "Usage: shift_subtitle --operation [ add | sub ] --time <time> <input_file> <output_file>"
+		@operation = 'add'
+	end
 end
--- spec/shift_subtitle_spec.rb
+++ spec/shift_subtitle_spec.rb
@@ -2,6 +2,19 @@ require File.expand_path(File.dirname(__FILE__) + '/../lib/shift_subtitle')
 
 describe ShiftSubtitle do
 	it "should print usage when called without parameters" do
-	
+		messenger = mock('messenger')
+		shift_subtitle = ShiftSubtitle.new(messenger)
+		messenger.should_receive(:puts).with("Usage: shift_subtitle --operation [ add | sub ] --time <time> <input_file> <output_file>" )
+		shift_subtitle.start	
+	end
+
+	it "should have 'add' operation when passed '--operation add'" do
+		messenger = mock('messenger')
+		shift_subtitle = ShiftSubtitle.new(messenger)
+		messenger.should_receive(:puts).with("Usage: shift_subtitle --operation [ add | sub ] --time <time> <input_file> <output_file>" )
+		argv =%w{--operation,add}
+		shift_subtitle.start argv
+		puts shift_subtitle.operation
+		shift_subtitle.operation.should =='add'
 	end
 end
