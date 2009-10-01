e35fc9896db170e7d03bafd7405dd2ca57c1947c
gscalzo <gscalzo@nkgscalzp2.urmetcns.it>
2009-10-01 23:50:04 +0200

    implemented optparse; need to implement handling all parameter

--- lib/shift_subtitle.rb
+++ lib/shift_subtitle.rb
@@ -1,12 +1,42 @@
+require 'optparse'
+
 class ShiftSubtitle
 	attr :operation
+	attr :time
+	attr :inputfile
+	attr :outputfile
+
 
 	def initialize(messenger)
 		@messenger = messenger
 	end
 
 	def start(argv=[])
-		@messenger.puts "Usage: shift_subtitle --operation [ add | sub ] --time <time> <input_file> <output_file>"
-		@operation = 'add'
+		options = {}
+		optparse = OptionParser.new do |o|
+			o.banner = "Usage: shift_subtitle [OPTIONS] <input_file> <output_file>"
+			o.separator   ""
+			o.on("-o", "--operation=[add|sub]", String,
+	       			"Type of operation: add or subtract.")   do |options[:operation]| end
+			o.on("-t", "--time=<time>", String,
+	       			"Time to shift in the format 'ss,mmm'")   do |options[:time]| end
+			o.separator   ""
+			o.on_tail("-h", "--help", "Show this help message.") do 
+				@messenger.puts o.to_s; 
+				return 1
+			end
+			o.parse!(argv)
+		end
+			
+		@operation = options[:operation]
+		@time = options[:time]
+		@inputfile = argv[0]
+		@outputfile = argv[1]
+		@messenger.puts optparse.to_s
+		return 1
 	end
+end
+if $0 == __FILE__
+	shift_subtitle = ShiftSubtitle.new(STDOUT)
+	shift_subtitle.start(ARGV)
 end
--- spec/shift_subtitle_spec.rb
+++ spec/shift_subtitle_spec.rb
@@ -1,20 +1,100 @@
+=begin rdoc
+  controllare che se tutti i parametri sono giusti il ritorno è 0
+	
+=end
 require File.expand_path(File.dirname(__FILE__) + '/../lib/shift_subtitle')
 
 describe ShiftSubtitle do
-	it "should print usage when called without parameters" do
-		messenger = mock('messenger')
-		shift_subtitle = ShiftSubtitle.new(messenger)
-		messenger.should_receive(:puts).with("Usage: shift_subtitle --operation [ add | sub ] --time <time> <input_file> <output_file>" )
-		shift_subtitle.start	
+	before(:each) do
+		@messenger = mock('messenger')
+		@shift_subtitle = ShiftSubtitle.new(@messenger)
 	end
 
-	it "should have 'add' operation when passed '--operation add'" do
-		messenger = mock('messenger')
-		shift_subtitle = ShiftSubtitle.new(messenger)
-		messenger.should_receive(:puts).with("Usage: shift_subtitle --operation [ add | sub ] --time <time> <input_file> <output_file>" )
-		argv =%w{--operation,add}
-		shift_subtitle.start argv
-		puts shift_subtitle.operation
-		shift_subtitle.operation.should =='add'
+	def do_should_print_usage
+		@messenger.should_receive(:puts).with(
+			"Usage: shift_subtitle [OPTIONS] <input_file> <output_file>\n" 			+
+			"\n" 										+
+			"    -o, --operation=[add|sub]        Type of operation: add or subtract.\n" 	+
+			"    -t, --time=<time>                Time to shift in the format 'ss,mmm'\n" 	+
+			"\n" 										+
+			"    -h, --help                       Show this help message.\n")
+		
+	end
+
+	def start_with(argv=[])
+		do_should_print_usage
+		@result = @shift_subtitle.start argv
+	end
+
+	context "starting up" do
+		after(:each) do
+			@result.should == 1
+		end
+
+		context "with '-h' paramenter"do
+			it "should print usage" do
+				start_with %w{-h}
+			end
+		end
+
+		context "without paramenters"do
+			it "should print usage" do
+				start_with 
+			end
+		end
+
+		context "with '--operation=add'"do
+			it "should have 'add' operation" do
+				start_with %w{--operation=add}
+				@shift_subtitle.operation.should =='add'
+			end
+		end
+
+		context "with '--operation=sub'"do
+			it "should have 'sub' operation" do
+				start_with %w{--operation=sub}
+				@shift_subtitle.operation.should =='sub'
+			end
+		end
+
+		context "with '--time=01,222'"do
+			it "should have '01,222' as time" do
+				start_with %w{--time=01,222}
+				@shift_subtitle.time.should =='01,222'
+			end
+		end
+
+		context "with 'infile.srt' as inputfile"do
+			it "should have 'infile.srt' as inputfile" do
+				start_with %w{infile.srt}
+				@shift_subtitle.inputfile.should =='infile.srt'
+			end
+		end
+	
+		context "with 'outfile.srt' as outputfile"do
+			it "should have 'outfile.srt' as outputfile" do
+				start_with %w{infile.srt, outfile.srt}
+				@shift_subtitle.outputfile.should =='outfile.srt'
+			end
+		end
+	end
+
+	context "starting up" do
+		context "with all parameters"do
+			before(:each) do
+				start_with %w{--operation=add --time=02,123 infile.srt outfile.srt}
+			end
+
+			it "should have all data" do
+				@shift_subtitle.operation.should =='add'
+				@shift_subtitle.time.should =='02,123'
+				@shift_subtitle.inputfile.should =='infile.srt'
+				@shift_subtitle.outputfile.should =='outfile.srt'
+			end
+			it "should return 0" do
+				@result.should == 0
+			end
+		end
+
 	end
 end
